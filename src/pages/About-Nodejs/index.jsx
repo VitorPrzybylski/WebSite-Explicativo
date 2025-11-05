7bad28679cf5c44014ef92c8d03fc96806bec2f3
VitorPrzybylski <vitorbibow@gmail.com>
2025-11-04 21:52:18 -0300

    feat

--- src/pages/About-Nodejs/index.jsx
+++ src/pages/About-Nodejs/index.jsx
@@ -23,18 +23,45 @@ function Nodejs() {
 
             </nav>
             <main>
-                <h3>NodeJs</h3>
+                <h3>NodeJs permite que o JavaScript seja executado fora do navegador, ou seja, no lado do servidor.</h3>
                 <p>
                     <h3>Inicio </h3>
                     <ul>
-                        <li> npm create vite@latest . --template react </li>
-                        <li> escolher React e depois JavaScript e digitar npm i </li>
+                        <li> instalar o node.js </li>
+                        <li> criar arquivos .js para poder usar o node.js </li>
+                        <li> rodar no terminal node nomeDoArquivo </li>
+                        <li> npm init para inicializar o projeto </li>
+                        <li> permite a abertura de um servidor </li>
+                        <li> possibilita uso do package.json </li>
+                        <li> Pode criar servidores HTTP sem frameworks </li>
                     </ul>
                     
-                    <h3>Inicio </h3>
+                    <h3>Express </h3>
                     <ul>
-                        <li> npm create vite@latest . --template react </li>
-                        <li> escolher React e depois JavaScript e digitar npm i </li>
+                        <li> framework para criar APIs e servidores web </li>
+                        <li> principal motivo para a escolha do Node.js </li>
+                        <li> npm install express </li>
+                        <li> gestao de rotas e configuraçoes de servidor  </li>
+                    </ul>
+                    <h3>Banco </h3>
+                    <ul>
+                        <li> node.js permite se conectar facilmente com bancos </li>
+                        <li> Acesso a MySQL e MongoDB </li>
+                        <li> possibilita de usar o sequelize para ORM e validaçao de dados </li>
+                    </ul>
+                    <h3>Async / Await </h3>
+                    <ul>
+                        <li> uma forma  simples de lidar com operações assíncronas no Node.js </li>
+                        <li> permite que coisas que demoram como consultar o banco ou chamar uma api, nao gerem falhas </li>
+                        <li> async marca uma função como assíncrona, retornando uma promise </li>
+                        <li> await espera a resposta antes de continuar o código  </li>
+                    </ul>
+                    <h3>SRC</h3>
+                    <ul>
+                        <li> responsavel para por guardas a maior parte das pastas </li>
+                        <li> permite que coisas que demoram como consultar o banco ou chamar uma api, nao gerem falhas </li>
+                        <li> async marca uma função como assíncrona, retornando uma promise </li>
+                        <li> await espera a resposta antes de continuar o código  </li>
                     </ul>
                 </p>
             </main>
